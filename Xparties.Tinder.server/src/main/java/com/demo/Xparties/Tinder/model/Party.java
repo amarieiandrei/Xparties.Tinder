b26b54045ef2cf4336fa858b7bc0dfce6bbb3e3a
Andrei Amariei <Andrei.Amariei.1116@hotmail.com>
2025-01-06 14:03:25 +0200

    first commit

---
+++ Xparties.Tinder.server/src/main/java/com/demo/Xparties/Tinder/model/Party.java
@@ -0,0 +1,9 @@
+package com.demo.Xparties.Tinder.model;
+
+public class Party {
+
+    private String id;
+    private String name;
+//    private ? members;
+//    TODO: Learning one-to-one & many-to-many & one-to-many relationships
+}
