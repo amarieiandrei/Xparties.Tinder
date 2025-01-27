c394456a73fbf92e247a10c13ac06534c6a86bb7
Andrei Amariei <Andrei.Amariei.1116@hotmail.com>
2025-01-27 19:57:36 +0200

    build updates

---
+++ Xparties.Tinder.app/src/main/resources/static/Xparties.Tinder-Frontend/webpack.extra.js
@@ -0,0 +1,7 @@
+module.exports = {
+  externals: {
+    // Mark the library/package as external so it won't be bundled
+    // "@spartan-ng/ui-button-helm": "UIButtonHelm",
+    "@spartan-ng/ui-core": "@spartan-ng/ui-core",
+  },
+};
