ec72f12d5eef655102f1b44210c22b0eb2dabb78
Andrei Amariei <Andrei.Amariei.1116@hotmail.com>
2025-01-29 18:40:38 +0200

    remove auth for deploying

--- Xparties.Tinder.app/src/main/java/com/demo/Xparties/Tinder/Security/SecurityConfig.java
+++ Xparties.Tinder.app/src/main/java/com/demo/Xparties/Tinder/Security/SecurityConfig.java
@@ -1,29 +1,29 @@
-package com.demo.Xparties.Tinder.Security;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class SecurityConfig {
-
-    private final CustomAuthenticationSuccessHandler successHandler;
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(auth -> {
-                    auth.anyRequest().authenticated();
-                })
-                .oauth2Login(oauth2 -> {
-                    oauth2.successHandler(successHandler);
-                })
-                .build();
-    }
-}
+//package com.demo.Xparties.Tinder.Security;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+//public class SecurityConfig {
+//
+//    private final CustomAuthenticationSuccessHandler successHandler;
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .authorizeHttpRequests(auth -> {
+//                    auth.anyRequest().authenticated();
+//                })
+//                .oauth2Login(oauth2 -> {
+//                    oauth2.successHandler(successHandler);
+//                })
+//                .build();
+//    }
+//}
