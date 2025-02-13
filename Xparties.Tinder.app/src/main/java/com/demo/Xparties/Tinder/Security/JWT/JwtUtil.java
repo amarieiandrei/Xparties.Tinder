0f180c29a17369596f194c86ca88b118a988df2f
Andrei Amariei <Andrei.Amariei.1116@hotmail.com>
2025-02-13 20:04:37 +0200

    a

--- Xparties.Tinder.app/src/main/java/com/demo/Xparties/Tinder/Security/JWT/JwtUtil.java
+++ Xparties.Tinder.app/src/main/java/com/demo/Xparties/Tinder/Security/JWT/JwtUtil.java
@@ -43,7 +43,7 @@ public class JwtUtil {
             cookie.setSecure(true); // Works only over HTTPS
             cookie.setPath("/");
             cookie.setMaxAge((int) (expirationTime / 1000) - 10);
-            cookie.setAttribute("SameSite", "Strict");
+//            cookie.setAttribute("SameSite", "Strict");
 
             response.addCookie(cookie);
 
@@ -116,7 +116,7 @@ public class JwtUtil {
             cookie.setSecure(true);
             cookie.setPath("/");
             cookie.setMaxAge(0);
-            cookie.setAttribute("SameSite", "Strict");
+//            cookie.setAttribute("SameSite", "Strict");
             response.addCookie(cookie);
 
         } catch (Exception e) {
