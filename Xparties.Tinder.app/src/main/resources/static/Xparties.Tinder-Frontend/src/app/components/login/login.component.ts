fe5e9d27123f3b960cef6ead3c0c580089ba1cca
Andrei Amariei <Andrei.Amariei.1116@hotmail.com>
2025-02-14 20:43:11 +0200

    frontend

--- Xparties.Tinder.app/src/main/resources/static/Xparties.Tinder-Frontend/src/app/components/login/login.component.ts
+++ Xparties.Tinder.app/src/main/resources/static/Xparties.Tinder-Frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { HlmButtonDirective } from '../../../../libs/ui/ui-button-helm/src/lib/hlm-button.directive';
 import { CommonModule } from '@angular/common';
@@ -9,4 +9,43 @@ import { CommonModule } from '@angular/common';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent { }
+export class LoginComponent implements OnInit {
+
+  data: any;  // To store the fetched data
+  errorMessage: string = '';  // To store any error message
+
+  constructor() { }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.fetchData();
+    }, 5000);
+  }
+
+  fetchData(): void {
+    const apiUrl = 'https://www.xpartiestinder.com/api/event/events';  // Replace with your API URL
+
+    fetch(apiUrl, {
+      method: 'GET',  // or 'POST', 'PUT', etc.
+      headers: {
+        'Content-Type': 'application/json'   // Set content type for request body if needed
+      },
+      credentials: 'include'
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();  // Parse JSON data
+      })
+      .then(data => {
+        this.data = data;  // Store the response data
+        console.log(this.data);
+      })
+      .catch(error => {
+        this.errorMessage = 'There was an error fetching the data: ' + error.message;
+        console.error('Error:', error);
+      });
+  }
+
+}
