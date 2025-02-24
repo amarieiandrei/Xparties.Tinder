39f684ab03f0c5618ee8f590576e8fee8bf87e05
Andrei Amariei <Andrei.Amariei.1116@hotmail.com>
2025-02-24 21:41:40 +0200

    123

--- Xparties.Tinder.app/src/main/java/com/demo/Xparties/Tinder/Model/Entity/User.java
+++ Xparties.Tinder.app/src/main/java/com/demo/Xparties/Tinder/Model/Entity/User.java
@@ -3,6 +3,10 @@ package com.demo.Xparties.Tinder.Model.Entity;
 import com.demo.Xparties.Tinder.Model.Enums.OAuth2Provider;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -31,6 +35,16 @@ public class User {
     @Column(name = "provider", nullable = false)
     @Enumerated(EnumType.STRING)
     private OAuth2Provider provider;
+
+    // TODO: REFACTOR WITH CUSTOMUSERDETAILS
+    // Add authorities as a transient field (not persisted in the database)
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
+
+    // Method to return authorities (empty collection)
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities != null ? authorities : Collections.emptyList();
+    }
 
     // TODO: I want to have the information stored in database not in name but in firstName and lastName
 //    @Column(name = "first_name", nullable = false)
