c0e0aca55109e8498ef751e80e283debb68541fa
Andrei Amariei <Andrei.Amariei.1116@hotmail.com>
2025-03-26 20:31:21 +0200

    123

--- Xparties.Tinder.app/src/main/resources/static/Xparties.Tinder-Frontend/src/app/components/dashboard/components/profile/edit-profile/edit-profile.component.ts
+++ Xparties.Tinder.app/src/main/resources/static/Xparties.Tinder-Frontend/src/app/components/dashboard/components/profile/edit-profile/edit-profile.component.ts
@@ -1,14 +1,23 @@
-import { Component, inject, OnDestroy, signal } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
 
 // animations
 import { slideFromBottomToTop } from '../../../../../shared/animations/slide-bottom-to-top.animation';
 
+// components
+import { HlmSwitchComponent } from '../../../../../../../libs/ui/ui-switch-helm/src/lib/hlm-switch.component';
+
 // directives
-import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+// import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmInputDirective } from '../../../../../../../libs/ui/ui-input-helm/src/lib/hlm-input.directive';
+import { HlmLabelDirective } from '../../../../../../../libs/ui/ui-label-helm/src/lib/hlm-label.directive';
 
 // icons
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { faArrowLeft, faPlus, faXmark } from '@fortawesome/free-solid-svg-icons';
+
+// imports
+import { BrnSelectImports } from '@spartan-ng/brain/select';
+import { HlmSelectImports } from '@spartan-ng/ui-select-helm';
 
 // services
 import { Router } from '@angular/router';
@@ -20,11 +29,18 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-edit-profile',
   imports: [
+    // components
+    HlmSwitchComponent,
     // directives
-    HlmButtonDirective,
+    // HlmButtonDirective,
+    HlmInputDirective,
+    HlmLabelDirective,
     // modules
     // CommonModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    // imports
+    BrnSelectImports,
+    HlmSelectImports,
   ],
   templateUrl: './edit-profile.component.html',
   animations: [slideFromBottomToTop]
@@ -37,6 +53,8 @@ export class EditProfileComponent implements OnDestroy {
 
   // services
   private _router = inject(Router);
+
+
 
   constructor() { }
 
