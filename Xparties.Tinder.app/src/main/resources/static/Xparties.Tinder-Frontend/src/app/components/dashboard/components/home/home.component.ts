653b54cb6ae28a3b7a6fb5515018ca5dbc501062
Andrei Amariei <Andrei.Amariei.1116@hotmail.com>
2025-06-08 15:22:05 +0300

    Asdasd

--- Xparties.Tinder.app/src/main/resources/static/Xparties.Tinder-Frontend/src/app/components/dashboard/components/home/home.component.ts
+++ Xparties.Tinder.app/src/main/resources/static/Xparties.Tinder-Frontend/src/app/components/dashboard/components/home/home.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
+
+// components
+import { HlmSkeletonComponent } from '../../../../../../libs/ui/ui-skeleton-helm/src/lib/hlm-skeleton.component';
+import { HlmSpinnerComponent } from '../../../../../../libs/ui/ui-spinner-helm/src/lib/hlm-spinner.component';
 
 // directives
 import { HlmButtonDirective } from '../../../../../../libs/ui/ui-button-helm/src/lib/hlm-button.directive';
@@ -14,6 +18,9 @@ import { faArrowRight, faCalendar, faLocationDot, faUsers } from '@fortawesome/f
 @Component({
   selector: 'xpt-home',
   imports: [
+    // components
+    HlmSkeletonComponent,
+    HlmSpinnerComponent,
     // directives
     HlmButtonDirective,
     // modules
@@ -23,12 +30,14 @@ import { faArrowRight, faCalendar, faLocationDot, faUsers } from '@fortawesome/f
   templateUrl: './home.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush // ✅ Improves performance
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   // icons
   faCalendar: IconDefinition = faCalendar;
   faLocationDot: IconDefinition = faLocationDot;
   faUsers: IconDefinition = faUsers;
   faArrowRight: IconDefinition = faArrowRight;
+
+  isBusy = signal<boolean>(true);
 
   enrolledEvents = signal<any>([
     { name: 'Beach Please 2025', location: 'Costinești, Romania', date: '2025-08-18', participants: 1200, src: '../../../../assets//photos-test/beach-please2.jpeg' },
@@ -37,6 +46,15 @@ export class HomeComponent {
     { name: 'Summer Music Festival', location: 'Barcelona, Spain', date: '2025-07-20', participants: 12200, src: '../../../../assets//photos-test/beach-please2.jpeg' },
     { name: 'Untold Festival', location: 'Cluj-Napoca, Romania', date: '2025-08-05', participants: 18200, src: '../../../../assets//photos-test/beach-please2.jpeg' },
     { name: 'Neversea', location: 'Mamaia, Romania', date: '2025-09-10', participants: 22222, src: '../../../../assets//photos-test/beach-please2.jpeg' },
+    { name: 'Neversea', location: 'Mamaia, Romania', date: '2025-09-10', participants: 22222, src: '../../../../assets//photos-test/beach-please2.jpeg' },
     { name: 'Vibe Festival', location: 'Targu Mures, Romania', date: '2025-10-03', participants: 9922, src: '../../../../assets//photos-test/snoop-dogg-dancing-exceed-limit-error-test.gif' },
   ])
+
+  constructor() { }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isBusy.set(false);
+    }, 2000);
+  }
 }
